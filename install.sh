ae1a8b389f104faa984f25666f45be8c2a1f853d
layfu <layfu@users.noreply.github.com>
2026-07-26 23:02:42 +0800

    bump version to 1.2.0.0

--- install.sh
+++ install.sh
@@ -31,7 +31,7 @@ OK="${Green}[OK]${Font}"
 Error="${Red}[错误]${Font}"
 
 # 版本
-shell_version="1.1.9.0"
+shell_version="1.2.0.0"
 shell_mode="None"
 github_branch="master"
 version_cmp="/tmp/version_cmp.tmp"
