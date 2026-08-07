4a96aa1b767379259b1e8e24d9e0c994010e717c
layfu <layfu@users.noreply.github.com>
2026-08-07 21:29:19 +0800

    audit fix: add sha256 checksum verification, fix hardcoded redirect domain

--- install.sh
+++ install.sh
@@ -595,7 +595,7 @@ nginx_conf_add() {
         listen 80;
         listen [::]:80;
         server_name serveraddr.com;
-        return 301 https://use.shadowsocksr.win\$request_uri;
+        return 301 https://${domain}\$request_uri;
     }
 EOF
 
