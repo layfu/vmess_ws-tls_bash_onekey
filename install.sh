84d94aec2af8da2f8e1fe243d44b56ae0c062109
layfu <layfu@users.noreply.github.com>
2026-08-29 22:32:33 +0800

    fix: embed sing-box version in custom build (constant.Version) so version check works, bump to 1.6.9.7

--- install.sh
+++ install.sh
@@ -31,7 +31,7 @@ OK="${Green}[OK]${Font}"
 Error="${Red}[错误]${Font}"
 
 # 版本
-shell_version="1.6.9.6"
+shell_version="1.6.9.7"
 shell_mode="None"
 github_branch="master"
 version_cmp="/tmp/version_cmp.tmp"
